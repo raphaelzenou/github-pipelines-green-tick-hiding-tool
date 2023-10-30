ec364930798fcfb61ef4861e5df5d0489e03883b
raphaelzenou <raphael.p.zenou@gmail.com>
2023-10-30 16:47:42 +0000

    Adding grey ticks

--- content_script.js
+++ content_script.js
@@ -8,14 +8,20 @@ function addImportantRule(element, property, value) {
 
 console.log(`Let's go 🚀`);
 
-var list = document.querySelectorAll(
+var list1 = document.querySelectorAll(
   ".octicon.octicon-check.mx-auto.d-block.color-fg-success"
 );
+var list2 = document.querySelectorAll(
+  ".octicon.octicon-skip.mx-auto.d-block.neutral-check"
+);
 
-if (!list.length) {
+if (!list1.length) {
   alert("Wrong URL, please visit a Github PR");
 } else {
-  var parentsListHidden = [...list].forEach(function (element, index) {
+  var parentsListHidden = [...list1, ...list2].forEach(function (
+    element,
+    index
+  ) {
     var parentElement = element.parentNode.parentNode;
     var parentClass = parentElement.className;
     var excludeClass = "review-status-item d-flex flex-justify-between";
