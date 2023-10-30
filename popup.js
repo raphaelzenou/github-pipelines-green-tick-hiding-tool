d0bc591c2eab6846fb2a18010e530bf223b3b740
raphaelzenou <raphael.p.zenou@gmail.com>
2023-10-30 16:51:26 +0000

    green and grey button id

--- popup.js
+++ popup.js
@@ -1,8 +1,10 @@
-document.getElementById("tryButton").addEventListener("click", function () {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id },
-      files: ["content_script.js"],
+document
+  .getElementById("greenAndGreyButton")
+  .addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        files: ["content_script.js"],
+      });
     });
   });
-});
